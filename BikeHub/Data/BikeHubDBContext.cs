db84dea457ce34ed0bd14da3eded85b4cc00bca8
Abarna <abarna.prema@gmail.com>
2024-03-06 17:59:38 -0500

    AwsDatabaseCreated and connected to microsoft SQL server and then connected to .net project

---
+++ BikeHub/Data/BikeHubDBContext.cs
@@ -0,0 +1,14 @@
+﻿using BikeHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeHub.Data
+{
+	public class BikeHubDBContext : DbContext
+    {
+        public BikeHubDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> CustomerDetails { get; set; }    
+    }
+}
---
+++ BikeHub/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BikeHub.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string CampusName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactNum { get; set; }
+        public string TypeOfCustomer { get; set; }
+        public string TypeOfRider { get; set; }
+        public string TAndCAgreement { get; set; }
+        public string EmailSubscription { get; set; }   
+
+
+    }
+}
+
+
